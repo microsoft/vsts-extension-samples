7669fd11dab3c29c866ceb9083a65cf2342d7c73
Ramya Achutha Rao <ramyar@microsoft.com>
2016-03-22 15:51:50 -0700

    Simplifying the examples

--- widgets/scripts/SayHello.js
+++ widgets/scripts/SayHello.js
@@ -4,45 +4,22 @@ VSS.init({
 });
 	   
 VSS.require("TFS/Dashboards/WidgetHelpers", function (WidgetHelpers) {
-	VSS.register("SampleTemplate1", function () {
-		WidgetHelpers.IncludeWidgetStyles();
+	WidgetHelpers.IncludeWidgetStyles();
+	
+	var registerWidget = function(){
 		return {
 			load: function (widgetSettings) {				
 				return WidgetHelpers.WidgetStatusHelper.Success();
 			}
 		}
-	});
-	VSS.register("SampleTemplate2", function () {
-		WidgetHelpers.IncludeWidgetStyles();
-		return {
-			load: function (widgetSettings) {				
-				return WidgetHelpers.WidgetStatusHelper.Success();
-			}
-		}
-	});
-		VSS.register("SampleTemplate3", function () {
-		WidgetHelpers.IncludeWidgetStyles();
-		return {
-			load: function (widgetSettings) {				
-				return WidgetHelpers.WidgetStatusHelper.Success();
-			}
-		}
-	});
-	VSS.register("SampleTemplate4", function () {
-		WidgetHelpers.IncludeWidgetStyles();
-		return {
-			load: function (widgetSettings) {				
-				return WidgetHelpers.WidgetStatusHelper.Success();
-			}
-		}
-	});
-		VSS.register("SampleTemplate5", function () {
-		WidgetHelpers.IncludeWidgetStyles();
-		return {
-			load: function (widgetSettings) {				
-				return WidgetHelpers.WidgetStatusHelper.Success();
-			}
-		}
-	});
+	}
+	
+	VSS.register("SampleTemplate1", registerWidget);
+	VSS.register("SampleTemplate2", registerWidget);
+	VSS.register("SampleTemplate3", registerWidget);
+	VSS.register("SampleTemplate4", registerWidget);
+	VSS.register("SampleTemplate5", registerWidget);
+	
+	
 	VSS.notifyLoadSucceeded();
 });
