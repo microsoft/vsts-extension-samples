1cebf2b04251b3b7b67c80b5b3af6082f3e49759
Trevor Gau <trgau@microsoft.com>
2016-12-09 10:53:31 -0500

    Update build-results-enhancer sample to use latest TypeScript and new VSS SDK.

--- build-results-enhancer/src/enhancer/status.ts
+++ build-results-enhancer/src/enhancer/status.ts
@@ -1,5 +1,3 @@
-/// <reference path='../../typings/main.d.ts' />
-
 import Controls = require("VSS/Controls");
 import VSS_Service = require("VSS/Service");
 import TFS_Build_Contracts = require("TFS/Build/Contracts");
