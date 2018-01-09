0b0cda3c64793924121b41021c3616805bc0a224
Ruslan Semenov <rsemenov@microsoft.com>
2018-01-09 12:40:42 -0500

    Add pull request statuses menu contribution samples

--- contributions-guide/scripts/main.js
+++ contributions-guide/scripts/main.js
@@ -2,22 +2,22 @@ var showPropertiesMenuProvider = (function () {
     "use strict";
     return {
         showPropertiesInDialog: function(properties, title) {
-            
+
             VSS.getService("ms.vss-web.dialog-service").then(function (dialogSvc) {
-                
+
                 var extInfo = VSS.getExtensionContext();
-                
+
                 var dialogOptions = {
                     title: title || "Properties",
                     width: 800,
                     height: 600,
                     buttons: null
                 };
-                
-                var contributionConfig = { 
+
+                var contributionConfig = {
                     properties: properties
                 };
-                
+
                 dialogSvc.openDialog(extInfo.publisherId + "." + extInfo.extensionId + "." + "contextForm", dialogOptions, contributionConfig);
             });
         },
@@ -29,4 +29,14 @@ var showPropertiesMenuProvider = (function () {
 
 VSS.register("showProperties", function (context) {
     return showPropertiesMenuProvider;
+});
+
+VSS.register("statusCustomAction", function (context) {
+    return {
+        execute: function (context) {
+            let statusName = context.status.context.genre + "/" + context.status.context.name;
+            let prId = context.pullRequest.pullRequestId;
+            alert("Execute custom action for status " + statusName + " on pull request " + prId);
+        }
+    };
 });
