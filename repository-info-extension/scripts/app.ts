b26af23672fb7d9350aeb198c5593b2ff42d9ce7
eldarerathis <eldarerathis@gmail.com>
2018-10-01 16:11:46 -0400

    Update HTML layout and screenshot

--- repository-info-extension/scripts/app.ts
+++ repository-info-extension/scripts/app.ts
@@ -2,9 +2,13 @@
 
 import TFS_VC_Services = require("TFS/VersionControl/Services");
 
-export async function execute(containerId: string) {
+export async function execute() {
     const svc = await TFS_VC_Services.VersionControlRepositoryService.getService();
     const repo = await svc.getCurrentGitRepository();
-    let container = document.getElementById(containerId);
-    container.innerHTML = JSON.stringify(repo);
+    let idContainer = document.getElementById("repository-id");
+    let nameContainer = document.getElementById("repository-name");
+    let urlContainer = document.getElementById("repository-url");
+    idContainer.innerHTML = repo.id;
+    nameContainer.innerHTML = repo.name;
+    urlContainer.innerHTML = repo.url;
 }
